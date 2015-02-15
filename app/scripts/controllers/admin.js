6924f09d0587b7d76e313ecf5c32c774eebfb427
divir94 <divir94@gmail.com>
2015-02-15 02:18:10 -0600

    more styling

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,20 +1,51 @@
-'use strict';
+var app = angular.module('barliftApp');
 
-/**
- * @ngdoc function
- * @name barliftApp.controller:AdminCtrl
- * @description
- * # AdminCtrl
- * Controller of the barliftApp
- */
-var admin = angular.module('barliftApp');
-
-admin.controller('AdminCtrl', function ($scope, Deals, AuthService) {
+app.controller('AdminCtrl', function ($scope, Deals, AuthService) {
+  // variables
   $scope.deals = [];
 
-  Deals.query(function(res){
-    $scope.deals = res;
-  });
+  // get all deals
+  Deals.query(function(deals) { $scope.deals = deals; });
 
+  // logout
   $scope.logout = AuthService.logout;
+});
+
+
+app.directive('navBar', function (){
+  return {
+    templateUrl: 'views/admin/nav-bar.html',
+    restrict: 'E'
+  };
+});
+
+app.directive('searchBar', function (){
+  return {
+    templateUrl: 'views/admin/search-bar.html',
+    restrict: 'E'
+  };
+});
+
+app.directive('dealOptions', function (){
+  return {
+    templateUrl: 'views/admin/deal-options.html',
+    restrict: 'E'
+  };
+});
+
+app.directive('dealGrid', function (){
+  return {
+    templateUrl: 'views/admin/deal-grid.html',
+    restrict: 'E'
+  };
+});
+
+app.directive('dealCard', function (){
+  return {
+    templateUrl: 'views/admin/deal-card.html',
+    restrict: 'E',
+    scope: {
+      deal: '=',
+    }
+  };
 });
