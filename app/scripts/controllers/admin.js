824e957b6eb76e482a396ef663e852a79dc77187
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-02-18 21:32:05 -0600

    added modal

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,11 +1,12 @@
 var app = angular.module('barliftApp');
 
-app.controller('AdminCtrl', function ($scope, Deals, AuthService) {
+app.controller('AdminCtrl', function ($scope, $modal, Deals, AuthService) {
   // variables
   $scope.deals = [];
 
   // get all deals
   Deals.query(function(deals) { $scope.deals = deals; });
+
 
   // logout
   $scope.logout = AuthService.logout;
--- app/scripts/directives/dealgrid.js
+++ app/scripts/directives/dealgrid.js
@@ -7,12 +7,17 @@
  * # dealGrid
  */
 angular.module('barliftApp')
-  .directive('dealGrid', function () {
+  .directive('dealGrid', function ($modal) {
     return {
       templateUrl: 'views/dealgrid.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-
-      }
+        scope.openDeal = function() {
+          console.log('opening');
+          var modalInstance = $modal.open({
+            template: '<h2>Hello</h2>'
+          });
+        };
+      } 
     };
   });
