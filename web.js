768b2f245d002b3b519b79e381133a813556ef33
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-05-15 11:47:15 -0500

    new build

--- web.js
+++ web.js
@@ -13,6 +13,7 @@ app.use(morgan('dev'));
 app.use("/fonts", gzippo.staticGzip(__dirname + "/dist/fonts"));
 app.use("/images", gzippo.staticGzip(__dirname + "/dist/images"));
 app.use("/js", gzippo.staticGzip(__dirname + "/dist/js"));
+app.use("/css", gzippo.staticGzip(__dirname + "/dist/css"));
 app.use("/scripts", gzippo.staticGzip(__dirname + "/dist/scripts"));
 app.use("/styles", gzippo.staticGzip(__dirname + "/dist/styles"));
 app.use("/views", gzippo.staticGzip(__dirname + "/dist/views"));
