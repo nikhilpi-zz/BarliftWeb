b78c210bf38102669bcf4247e41d4d699843f460
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-05-25 01:47:55 -0500

    pricing:

--- app/scripts/controllers/adminview.js
+++ app/scripts/controllers/adminview.js
@@ -8,10 +8,13 @@
  * Controller of the barliftApp
  */
 angular.module('barliftApp')
-  .controller('AdminviewCtrl', function ($scope, Deals, Venues, $q, Session, AuthService) {
+  .controller('AdminviewCtrl', function ($scope,CloudCode, Deals, User, Venues, $q, Session, AuthService, $http) {
     var cellEditableTemplate = "<input ng-class=\"'colt' + col.index\" ng-input=\"COL_FIELD\" ng-model=\"COL_FIELD\" ng-change=\"updateEntity(row.entity)\"/>";
     $scope.selectedDeal = [];
+    $scope.pricing = [];
     $scope.deals = [];
+    $scope.users = [];
+    $scope.alert = {};
     $scope.role = Session.userRole;
     $scope.ngOptions = { 
       enableRowSelection: true,
@@ -27,6 +30,15 @@ angular.module('barliftApp')
       ],
       selectedItems: $scope.selectedDeal,
     };
+
+    $scope.getDay = function(num){
+      return moment().day(num).format('dddd');
+    }
+
+    $http.get('https://api.parse.com/1/config').
+      success(function(data, status, headers, config) {
+        $scope.pricing = data.params.pricing;
+    });
 
     $scope.updateEntity = function(ent){
       console.log(ent);
@@ -47,6 +59,28 @@ angular.module('barliftApp')
       });
 
     });
+
+    User.query({
+      where: {
+        Role: {
+          __type: 'Pointer',
+          className: '_Role',
+          objectId: 'VyofaLB7t2'
+        } 
+      }
+    }, function(users){
+      $scope.users = users;
+    })
+
+    $scope.updateSub = function(){
+      angular.forEach($scope.users, function(user){
+        if(user.sub_price){
+          CloudCode.call('updateSub', {userID: user.objectId, sub_price: user.sub_price});
+        }
+      });
+
+      $scope.$emit('notify', {cssClass: 'alert-success', message:'Subscription pricing has been saved'});
+    };
 
     $scope.logout = AuthService.logout;
 
